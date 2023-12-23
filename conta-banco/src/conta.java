a2ff1aa7391a15f2682af8fb4e12c0f754d876c9
LuisGustavo18 <luisgustavopires75@gmail.com>
2023-12-22 22:03:28 -0300

    meu projeto conta banco

---
+++ conta-banco/src/conta.java
@@ -0,0 +1,11 @@
+public class conta {
+    
+    int numero = 1021;
+    String agencia = "6070";
+    String nome = "Luis Gustavo";
+    double saldo = 1500;
+    public Object agenciaa;
+
+   
+
+}
